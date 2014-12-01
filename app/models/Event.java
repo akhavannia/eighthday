3f1b36923d8953db02f826ef025d8d7ca4b2fc01
Benjamin Kazemi <Benjamin-Kazemi@uiowa.edu>
2014-11-30 20:40:29 -0600

    fix

--- app/models/Event.java
+++ app/models/Event.java
@@ -2,9 +2,12 @@ package models;
 
 import play.db.jpa.Model;
 
+import javax.persistence.Entity;
+
 /**
  * Created by bkazemi on 11/23/2014.
  */
+@Entity
 public class Event extends Model {
     public String tag;
     public String summary;
